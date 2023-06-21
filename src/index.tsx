310414d0f961906340e47101cb5abf13459aa98d
zhanetfio <zhanet_f@mail.ru>
2023-06-21 18:52:39 +0300

    feat: add Global styles

--- src/index.tsx
+++ src/index.tsx
@@ -3,14 +3,20 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import {GlobalStyles} from "./styles/GlobalStyles";
+import {ThemeProvider} from "styled-components";
+import {myTheme} from "./styles/Theme.styled";
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+
+    <ThemeProvider theme={myTheme}>
+        <App/>
+        <GlobalStyles/>
+    </ThemeProvider>
+
 );
 
 // If you want to start measuring performance in your app, pass a function
